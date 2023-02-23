13d0395a3554180cf1f2c05b4d6fe401ed2a04aa
Jorgen Evens <jorgen@evens.eu>
2023-02-23 01:14:25 +0100

    fix(home): add way to navigate to users

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
-        <slot />
+        <div>
+            <h2>View User</h2>
+            <input type="text" v-model="username" />
+            <input type="button" value="View" @click="viewUser">
+        </div>
+        <div>
+            <h2>View Project</h2>
+            <input type="text" v-model="project" />
+            <input type="button" value="View" @click="viewProject">
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+
+    data() {
+        return {
+            username: 'JorgenEvens',
+            project: 'JorgenEvens/rest-store'
+        };
+    },
+
+    methods: {
+        viewUser() {
+            this.$router.push({
+                name: 'user',
+                params: { userId: this.username },
+            });
+        },
+        viewProject() {
+            this.$router.push({
+                name: 'project',
+                params: { projectId: this.project },
+            });
+        }
+    }
+
 
 };
 </script>
